aad2b8b455600ed630ffc377b7492b0e84177364
Jonathan Guidi <jguidi.info@gmail.com>
2023-09-28 16:12:53 -0300

    Project section  responsive

--- src/components/components_EN/Projects.jsx
+++ src/components/components_EN/Projects.jsx
@@ -17,9 +17,9 @@ const Projects = () => {
   return (
     <section className="max-w-[1220px] mx-auto py-20 max-[860px]:px-14 max-[480px]:px-7">
 
-      <div className="max-w-[838px] mx-auto flex flex-col justify-center items-center">
+      <div className="max-w-[838px] pb-6 mx-auto flex flex-col justify-center items-center">
         <p className="badge ml-2 py-3 px-6 text-[12px] text-center font-semibold text-white mb-2 max-[455px]:scale-[0.88]">OUR PROJECTS</p>
-        <h2 className="max-w-[888px] text-[#FCFCFD] leading-[48px] font-bold text-[42px] text-center pb-3 max-[1024px]:text-[32px] max-[455px]:text-[24px] max-[455px]:leading-8">
+        <h2 className="max-w-[888px] text-[#FCFCFD] leading-[58px] font-bold text-[52px] text-center pb-3 max-[1024px]:text-[32px] max-[1024px]:leading-[40px] max-[455px]:text-[24px] max-[455px]:leading-8">
           Come for the features.<br></br>
           Stay for best ever help.
         </h2>
@@ -35,11 +35,11 @@ const Projects = () => {
       </div>
 
 
-      <div id="projects" className="flex items-center justify-around py-20">
+      <div id="projects" className="flex items-center justify-center py-10 max-[1210px]:scale-90 max-[1040px]:flex-col max-[1040px]:scale-100 m" >
 
-        <div className="max-w-[546px] flex flex-col gap-10">
+        <div className="max-w-[546px] flex flex-col flex-end gap-10 max-[1440px]:scale-[0.90] max-[1040px]:scale-[0.85]">
 
-          <div className="max-w-[467px]">
+          <div className="max-w-[487px]">
             <img src={Future} alt="Future" className="pb-6" />
             <p className="text-[12px] text-white font-semibold uppercase pb-3">Transformando educación - Innovación educativa digital</p>
             <p className="text-[16px] font-normal text-[#868687] pb-6">FutureCard Visa® Debit: Cashback for eco-friendly spending. Earn 6% at FuturePartners and 5% on green choices like public transport, electric charging, and sustainable products. Future empowers you to make climate-conscious decisions, impacting 66% of carbon emissions. Join Future and earn FutureCoins through app Missions.</p>
@@ -50,10 +50,10 @@ const Projects = () => {
             </button>
           </div>
 
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-6 ">
             <div className="flex flex-col items-start gap-3">
               <p className="text-[12px] font-semibold">AWARDS AND WINNING</p>
-              <div className="flex gap-5">
+              <div className="flex gap-5 flex-wrap">
                 <button className="badge flex items-center gap-2">
                   <span className="text-[18px] mb-[2px]"><BsFillCreditCard2BackFill /></span>
                   VISA PARTNER
@@ -66,7 +66,7 @@ const Projects = () => {
             </div>
             <div className="flex flex-col items-start gap-3">
               <p className="text-[12px] font-semibold">TECH STACK</p>
-              <div className="flex gap-5">
+              <div className="flex gap-5 flex-wrap">
                 <button className="badge flex items-center gap-2">
                   <span className="text-[18px] mb-[2px]"><FaReact /></span>
                   REACT
@@ -89,7 +89,7 @@ const Projects = () => {
 
           <div className="flex flex-col gap-4">
             <p className="text-[14px] text-[#A1A1A9]">Who supports us</p>
-            <div className="flex gap-4">
+            <div className="flex gap-4 flex-wrap">
               <div className="flex justify-center items-center gap-2">
                 <img src={iosIcon} alt="iOS" />
                 <p className="text-[16px] text-white font-bold">iOS Download</p>
@@ -108,18 +108,18 @@ const Projects = () => {
         </div>
 
         <div>
-          <img className="scale-110 pb-10" src={FutureImage} width={674} height={708} />
+          <img className="scale-105 pb-0 max-[1240px]:scale-90 " src={FutureImage} width={674} height={708} />
         </div>
 
       </div>
 
-      <div id="Bombo_Project" className="flex items-center justify-around py-20">
+      <div id="Bombo_Project" className="flex items-center justify-around py-10 max-[1440px]:scale-[0.90] max-[1240px]:py-16 max-[1040px]:flex-col max-[1040px]:scale-100 max-[1040px]:gap-8 max-[1040px]:flex-col-reverse max-[500px]:scale-[0.85]">
         <div>
-          <img className="scale-125 pb-10" src={BomboImage} width={605} height={556} />
+          <img className="scale-125" src={BomboImage} width={605} height={556} />
         </div>
         <div className="max-w-[546px] flex flex-col gap-10">
 
-          <div className="max-w-[482px]">
+          <div className="max-w-[482px] flex-wrap max-[1210px]:scale-90 ">
             <img src={Bombo} alt="Bombo" className="pb-6" />
             <p className="text-[12px] text-white font-semibold uppercase pb-3">Revolucionando la industria de ticketing con tecnología blockchain.</p>
             <p className="text-[16px] font-normal text-[#A1A1A9] pb-6">Aplicación móvil, combinada con una plataforma administrativa de backend, ofrece una solución segura, transparente y eficiente para la gestión y venta de boletos utilizando la tecnología NFT y blockchain.</p>
@@ -130,10 +130,10 @@ const Projects = () => {
             </button>
           </div>
 
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-6 flex-wrap max-[1210px]:scale-90">
             <div className="flex flex-col items-start gap-3">
               <p className="text-[12px] font-semibold">AWARDS AND WINNING</p>
-              <div className="flex gap-5">
+              <div className="flex gap-5 flex-wrap">
                 <button className="badge flex justify-center items-center gap-2 uppercase">
                   <span className="text-[18px] mb-1"><BsApple /></span>
                   Top #1 AppStore Downloads
@@ -147,7 +147,7 @@ const Projects = () => {
             </div>
             <div className="flex flex-col items-start gap-3">
               <p className="text-[12px] font-semibold">TECH STACK</p>
-              <div className="flex gap-5">
+              <div className="flex gap-5 flex-wrap">
                 <button className="badge flex items-center gap-2">
                   <span className="text-[18px] mb-[2px]"><SiEthereum /></span>
                   SOLIDITY
@@ -168,9 +168,9 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 max-[1210px]:scale-90">
             <p className="text-[14px] text-[#A1A1A9]">Who supports us</p>
-            <div className="flex gap-4">
+            <div className="flex gap-4 flex-wrap">
               <div className="flex justify-center items-center gap-2">
                 <img src={iosIcon} alt="iOS" />
                 <p className="text-[16px] text-white font-bold">iOS Download</p>
